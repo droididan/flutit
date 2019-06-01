d3a0dde532040a5e61fc8245402807863e516fbb
Idan Ayalon <idan@michals-MacBook-Pro.local>
2019-06-01 11:42:15 +0300

    init

---
+++ android/app/src/main/kotlin/com/it/flut/flut_it/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.it.flut.flut_it
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
