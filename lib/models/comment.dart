873f0115d69459406d563d01bfd7130d56ed9bd8
Erez Bursztyn <bursztyn10+github@gmail.com>
2019-06-01 12:33:39 +0300

    feed+comment models

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -1,3 +1,11 @@
 class Comment {
 
+  DateTime _timestamp;
+  String _authorId;
+  String _content;
+
+  DateTime get timestamp => _timestamp;
+  String get content => _content;
+  String get authorId => _authorId;
+
 }
--- lib/models/feed.dart
+++ lib/models/feed.dart
@@ -6,10 +6,12 @@ class Feed {
   String _content;
   String _authorId;
   List<Comment> _comments;
+  List<String> _likes;
 
   String get title => _title;
   String get content => _content;
   String get authorId => _authorId;
   List<Comment> get tokens => _comments;
+  List<String> get likes => _likes;
 
 }
