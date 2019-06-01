dbf177c3572f75aaf453395a38e00372763073b5
Erez Bursztyn <bursztyn10+github@gmail.com>
2019-06-01 16:58:54 +0300

    profile mock

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child:
+          Column(children: <Widget>[
+            SizedBox(height: 90),
+            CircleAvatar(
+              radius: 130,
+              backgroundColor: Colors.greenAccent,
+              child: Image.asset("lib/assets/images/profile.png"),
+            ),
+            Text("Flutter Programmer", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),)
+          ],)
+
+    );
   }
 }
