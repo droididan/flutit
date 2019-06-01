6e715fff325fe98bb989136d27104c2404518a72
Erez Bursztyn <bursztyn10+github@gmail.com>
2019-06-01 15:16:55 +0300

    wizard wip

--- lib/screens/WizardScreen.dart
+++ lib/screens/WizardScreen.dart
@@ -7,8 +7,43 @@ class WizardScreen extends StatefulWidget {
 }
 
 class _WizardScreenState extends State<WizardScreen> {
+
+  String selectedSkill = "Beginner";
+
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Scaffold (
+      backgroundColor: Colors.white,
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 250.0),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter your name",
+                    icon: Icon(Icons.person),
+                  ),
+                ),
+                DropdownButton<String>(
+                  value: selectedSkill,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      selectedSkill = newValue;
+                    });
+                  },
+                  items: <String>['Beginner', 'Intermediate', 'Expert']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                )
+              ],
+            )
+          ),
+        )
+    );
   }
 }
